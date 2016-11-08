5ac02419ad9b379d91125809e10fd53032647732
Wenyan Deng <viviandwy@wireless-s0-no-150-7-234.uchicago.edu>
2016-11-07 18:12:07 -0600

    done

--- e2.py
+++ e2.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-files = "http://historical.elections.virginia.gov/elections/download/{}/precincts_include:0/"
-resp = requests.get(files)
-soup = bs(resp.content , "html.parser")
-
 for line in open("ELECTION_ID"):
     a = line.split()
-    file_name = a[0] + ".csv"
     files = "http://historical.elections.virginia.gov/elections/download/{}/precincts_include:0/".format(a[1])
+    resp = requests.get(files)
+    soup = bs(resp.content , "html.parser")
+    file_name = a[0] + ".csv"
     with open(file_name, "w") as out:
         out.write(resp.text)
